fcd903e5ac8654e92ddb69b44187e02d54aa1196
hikee9123 <77569237+hikee9123@users.noreply.github.com>
2021-07-14 00:00:35 +0900

    Update safety_defaults_HKG.h

--- panda/board/safety/safety_defaults_HKG.h
+++ panda/board/safety/safety_defaults_HKG.h
@@ -9,6 +9,8 @@ int HKG_MDPS12_checksum = -1;
 int HKG_MDPS12_cnt = 0;
 int HKG_last_StrColT = 0;
 
+// modify  
+// Modify  by atom
 
 int default_rx_hook(CAN_FIFOMailBox_TypeDef *to_push) {
   int bus = GET_BUS(to_push);
