34b264430c02aa85d0d24590099f2ff4970d30b0
hikee9123 <hikee9123@gmail.com>
2021-07-16 23:44:07 +0900

    dd

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -569,7 +569,7 @@ class Controls:
       # send car controls over can
       can_sends = self.CI.apply(CC)
       if CS.cruiseState.enabled:
-        self.pm.send('sendcan', can_list_to_can_capnp(can_sends, msgtype='sendcan', valid=CS.canValid))
+         self.pm.send('sendcan', can_list_to_can_capnp(can_sends, msgtype='sendcan', valid=CS.canValid))
 
     force_decel = (self.sm['driverMonitoringState'].awarenessStatus < 0.) or \
                   (self.state == State.softDisabling)
