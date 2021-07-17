a173f61acdcd2a6f6e8afea76218c5fc65a64308
hikee9123 <hikee9123@gmail.com>
2021-07-18 08:32:00 +0900

    dd

--- selfdrive/ui/qt/atom/userPanel.cc
+++ selfdrive/ui/qt/atom/userPanel.cc
@@ -254,6 +254,19 @@ void CUserPanel::showEvent(QShowEvent *event)
 
 
 
+
+IsOpenpilotViewEnabledToggle::IsOpenpilotViewEnabledToggle() 
+        : ToggleControl("주행화면 미리보기", "오픈파일럿 주행화면을 미리보기 합니다.", "", Params().getBool("IsOpenpilotViewEnabled")) 
+{
+  QObject::connect(this, &IsOpenpilotViewEnabledToggle::toggleFlipped, [=](int state) {
+    char value = state ? '1' : '0';
+
+    QUIState::ui_state.scene.IsOpenpilotViewEnabled = state;
+    Params().put("IsOpenpilotViewEnabled", &value, 1);
+  });
+}
+
+
 ////////////////////////////////////////////////////////////////////////////////////////
 //
 //  CLiveSteerRatioToggle
