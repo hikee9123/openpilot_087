3d904354d0f60159058457d73745e5864cd87b32
hikee9123 <hikee9123@gmail.com>
2021-07-17 21:28:06 +0900

    dd

--- selfdrive/ui/qt/offroad/settings.cc
+++ selfdrive/ui/qt/offroad/settings.cc
@@ -344,7 +344,7 @@ SettingsWindow::SettingsWindow(QWidget *parent) : QFrame(parent) {
     QObject::connect(map_panel, &MapPanel::closeSettings, this, &SettingsWindow::closeSettings);
   }
 #endif
-  const int padding = panels.size() > 3 ? 25 : 35;
+  const int padding = panels.size() > 3 ? 15 : 35;
 
   nav_btns = new QButtonGroup();
   for (auto &[name, panel] : panels) {
