faf89f25e218f28924e187025b1afb62198280f7
hikee9123 <hikee9123@gmail.com>
2021-07-23 19:11:30 +0900

    openpilot 0.8.6

--- selfdrive/car/hyundai/carcontroller.py
+++ selfdrive/car/hyundai/carcontroller.py
@@ -10,29 +10,6 @@ from selfdrive.car.hyundai.navicontrol  import NaviControl
 VisualAlert = car.CarControl.HUDControl.VisualAlert
 LaneChangeState = log.LateralPlan.LaneChangeState
 
-def process_hud_alert(enabled, fingerprint, visual_alert, left_lane,
-                      right_lane, left_lane_depart, right_lane_depart):
-  sys_warning = (visual_alert in [VisualAlert.steerRequired, VisualAlert.ldw])
-
-  # initialize to no line visible
-  sys_state = 1
-  if left_lane and right_lane or sys_warning:  # HUD alert only display when LKAS status is active
-    sys_state = 3 if enabled or sys_warning else 4
-  elif left_lane:
-    sys_state = 5
-  elif right_lane:
-    sys_state = 6
-
-  # initialize to no warnings
-  left_lane_warning = 0
-  right_lane_warning = 0
-  if left_lane_depart:
-    left_lane_warning = 1 if fingerprint in [CAR.GENESIS_G90, CAR.GENESIS_G80] else 2
-  if right_lane_depart:
-    right_lane_warning = 1 if fingerprint in [CAR.GENESIS_G90, CAR.GENESIS_G80] else 2
-
-  return sys_warning, sys_state, left_lane_warning, right_lane_warning
-
 
 class CarController():
   def __init__(self, dbc_name, CP, VM):
@@ -47,14 +24,53 @@ class CarController():
     self.last_lead_distance = 0
     self.lkas11_cnt = 0
 
-    self.navi_ctrl = NaviControl(self.p)
+    self.NC = NaviControl(self.p)
     self.steerWarning_time = 0
     self.steer_torque_wait_timer = 0
     self.blinker_safety_timer = 0
 
+    # hud
+    self.hud_timer_left = 0
+    self.hud_timer_right = 0
+
+
+  def process_hud_alert(self, enabled, c ):
+    visual_alert = c.hudControl.visualAlert
+    left_lane = c.hudControl.leftLaneVisible
+    right_lane = c.hudControl.rightLaneVisible
+
+    sys_warning = (visual_alert in [VisualAlert.steerRequired, VisualAlert.ldw])
+
+    if left_lane:
+      self.hud_timer_left = 100
+
+    if right_lane:
+      self.hud_timer_right = 100
+
+    if self.hud_timer_left:
+      self.hud_timer_left -= 1
+ 
+    if self.hud_timer_right:
+      self.hud_timer_right -= 1
+
+
+    # initialize to no line visible
+    sys_state = 1
+    if self.hud_timer_left and self.hud_timer_right or sys_warning:  # HUD alert only display when LKAS status is active
+      if enabled or sys_warning:
+        sys_state = 3
+      else:
+        sys_state = 4
+    elif self.hud_timer_left:
+      sys_state = 5
+    elif self.hud_timer_right:
+      sys_state = 6
+
+    return sys_warning, sys_state
+
+
   def lkas_active_control( self, enabled, CS, path_plan ):
     steerAngleDegAbs = abs(CS.out.steeringAngleDeg)
-
     
     if CS.out.steerWarning:
       self.steerWarning_time = 200
@@ -81,11 +97,10 @@ class CarController():
     if self.steer_torque_wait_timer > 0:
       self.steer_torque_wait_timer -= 1   
     
-    
     lkas_active = enabled and not self.steerWarning_time and CS.out.vEgo >= CS.CP.minSteerSpeed and CS.out.cruiseState.enabled
     lkas_active = lkas_active and self.steer_torque_wait_timer == 0
-
     return lkas_active
+  
 
   def update(self, c, CS, frame ):
     enabled = c.enabled
@@ -94,8 +109,8 @@ class CarController():
     visual_alert = c.hudControl.visualAlert
     left_lane = c.hudControl.leftLaneVisible 
     right_lane = c.hudControl.rightLaneVisible 
-    left_lane_depart = c.hudControl.leftLaneDepart 
-    right_lane_depart = c.hudControl.rightLaneDepart
+    left_lane_warning = c.hudControl.leftLaneDepart 
+    right_lane_warning = c.hudControl.rightLaneDepart
     
     # Steering Torque
     new_steer = int(round(actuators.steer * self.p.STEER_MAX))
@@ -104,7 +119,7 @@ class CarController():
 
     # disable when temp fault is active, or below LKA minimum speed
     #lkas_active = enabled and not CS.out.steerWarning and CS.out.vEgo >= CS.CP.minSteerSpeed and CS.out.cruiseState.enabled
-    path_plan = self.navi_ctrl.update_lateralPlan()    
+    path_plan = self.NC.update_lateralPlan()    
     lkas_active = self.lkas_active_control( enabled, CS, path_plan )
 
     if not lkas_active:
@@ -112,14 +127,12 @@ class CarController():
 
     self.apply_steer_last = apply_steer
 
-    sys_warning, sys_state, left_lane_warning, right_lane_warning = \
-      process_hud_alert(lkas_active, self.car_fingerprint, visual_alert,
-                        left_lane, right_lane, left_lane_depart, right_lane_depart)
+    sys_warning, sys_state = self.process_hud_alert( lkas_active, c )
+
 
 
     if frame == 0: # initialize counts from last received count signals
       self.lkas11_cnt = CS.lkas11["CF_Lkas_MsgCount"] + 1
-
     self.lkas11_cnt %= 0x10
 
     can_sends = []
@@ -151,7 +164,7 @@ class CarController():
       self.last_lead_distance = 0
 
     elif CS.out.cruiseState.accActive:
-      btn_signal = self.navi_ctrl.update( CS )
+      btn_signal = self.NC.update( CS )
       if btn_signal != None:
         can_sends.append(create_clu11(self.packer, self.resume_cnt, CS.clu11, btn_signal ))
         self.resume_cnt += 1
